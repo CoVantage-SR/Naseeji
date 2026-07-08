a536429dd4287d06be97d3fed5aa7db6f98e466a
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-07-08 21:46:20 +0300

    feat: generate Riverpod providers and router configuration for financial module features

--- naseeji_supplier/lib/core/router/app_router.g.dart
+++ naseeji_supplier/lib/core/router/app_router.g.dart
@@ -6,7 +6,7 @@ part of 'app_router.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$goRouterHash() => r'bb7dbe0ac8827949cf5f0dd701817f53a90c3ed9';
+String _$goRouterHash() => r'143d309ffc37c83965aa7b47e79554e139e870b4';
 
 /// See also [goRouter].
 @ProviderFor(goRouter)
---
+++ naseeji_supplier/lib/features/financial/presentation/controllers/financial_controllers.g.dart
@@ -0,0 +1,411 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'financial_controllers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$financialDashboardControllerHash() =>
+    r'e44e99b0930c05a78734d351d8272ba9cb5b7ce2';
+
+/// See also [FinancialDashboardController].
+@ProviderFor(FinancialDashboardController)
+final financialDashboardControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialDashboardController,
+      FinancialDashboardData
+    >.internal(
+      FinancialDashboardController.new,
+      name: r'financialDashboardControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialDashboardControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialDashboardController =
+    AutoDisposeAsyncNotifier<FinancialDashboardData>;
+String _$financialTransactionsControllerHash() =>
+    r'53e04d4e25cc19eb66560b8a92e7ab465b0963a7';
+
+/// See also [FinancialTransactionsController].
+@ProviderFor(FinancialTransactionsController)
+final financialTransactionsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialTransactionsController,
+      List<FinancialTransaction>
+    >.internal(
+      FinancialTransactionsController.new,
+      name: r'financialTransactionsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialTransactionsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialTransactionsController =
+    AutoDisposeAsyncNotifier<List<FinancialTransaction>>;
+String _$financialPaymentsControllerHash() =>
+    r'2a42be4f3a41540814515e3ca0258bdc3fc10a6a';
+
+/// See also [FinancialPaymentsController].
+@ProviderFor(FinancialPaymentsController)
+final financialPaymentsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialPaymentsController,
+      List<SupplierPayment>
+    >.internal(
+      FinancialPaymentsController.new,
+      name: r'financialPaymentsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialPaymentsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialPaymentsController =
+    AutoDisposeAsyncNotifier<List<SupplierPayment>>;
+String _$financialWalletControllerHash() =>
+    r'32b865daad9e232377ab023311265ccbd3dd507e';
+
+/// See also [FinancialWalletController].
+@ProviderFor(FinancialWalletController)
+final financialWalletControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialWalletController,
+      WalletData
+    >.internal(
+      FinancialWalletController.new,
+      name: r'financialWalletControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialWalletControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialWalletController = AutoDisposeAsyncNotifier<WalletData>;
+String _$financialWithdrawalsControllerHash() =>
+    r'3aab5fca1921003e5977feae7de3cef466a5f3fe';
+
+/// See also [FinancialWithdrawalsController].
+@ProviderFor(FinancialWithdrawalsController)
+final financialWithdrawalsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialWithdrawalsController,
+      List<WithdrawalRequest>
+    >.internal(
+      FinancialWithdrawalsController.new,
+      name: r'financialWithdrawalsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialWithdrawalsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialWithdrawalsController =
+    AutoDisposeAsyncNotifier<List<WithdrawalRequest>>;
+String _$financialInvoicesControllerHash() =>
+    r'323e037bde64df883993a936d25f01ab131b148a';
+
+/// See also [FinancialInvoicesController].
+@ProviderFor(FinancialInvoicesController)
+final financialInvoicesControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialInvoicesController,
+      List<SupplierInvoice>
+    >.internal(
+      FinancialInvoicesController.new,
+      name: r'financialInvoicesControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialInvoicesControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialInvoicesController =
+    AutoDisposeAsyncNotifier<List<SupplierInvoice>>;
+String _$financialAnalyticsControllerHash() =>
+    r'771b6b3314d058c9f06f6398042b241aa42641b8';
+
+/// See also [FinancialAnalyticsController].
+@ProviderFor(FinancialAnalyticsController)
+final financialAnalyticsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialAnalyticsController,
+      FinancialAnalyticsData
+    >.internal(
+      FinancialAnalyticsController.new,
+      name: r'financialAnalyticsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialAnalyticsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialAnalyticsController =
+    AutoDisposeAsyncNotifier<FinancialAnalyticsData>;
+String _$financialTaxCenterControllerHash() =>
+    r'48b0af7a8f300ae9123e292db6832e88f6cbba66';
+
+/// See also [FinancialTaxCenterController].
+@ProviderFor(FinancialTaxCenterController)
+final financialTaxCenterControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialTaxCenterController,
+      TaxCenterData
+    >.internal(
+      FinancialTaxCenterController.new,
+      name: r'financialTaxCenterControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialTaxCenterControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialTaxCenterController =
+    AutoDisposeAsyncNotifier<TaxCenterData>;
+String _$financialPaymentMethodsControllerHash() =>
+    r'3f06b9f4035ac7448115a659bcd87e0691ef5b1a';
+
+/// See also [FinancialPaymentMethodsController].
+@ProviderFor(FinancialPaymentMethodsController)
+final financialPaymentMethodsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialPaymentMethodsController,
+      List<PaymentMethod>
+    >.internal(
+      FinancialPaymentMethodsController.new,
+      name: r'financialPaymentMethodsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialPaymentMethodsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialPaymentMethodsController =
+    AutoDisposeAsyncNotifier<List<PaymentMethod>>;
+String _$financialRefundsControllerHash() =>
+    r'c3e46408cf0d416bb039d7dddc6eebe11a828d48';
+
+/// See also [FinancialRefundsController].
+@ProviderFor(FinancialRefundsController)
+final financialRefundsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialRefundsController,
+      List<RefundRequest>
+    >.internal(
+      FinancialRefundsController.new,
+      name: r'financialRefundsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialRefundsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialRefundsController =
+    AutoDisposeAsyncNotifier<List<RefundRequest>>;
+String _$financialReportsControllerHash() =>
+    r'74e35604f1e79082676963fe8f5a48ea30c1f4bd';
+
+/// See also [FinancialReportsController].
+@ProviderFor(FinancialReportsController)
+final financialReportsControllerProvider =
+    AutoDisposeAsyncNotifierProvider<
+      FinancialReportsController,
+      List<FinancialReport>
+    >.internal(
+      FinancialReportsController.new,
+      name: r'financialReportsControllerProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$financialReportsControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$FinancialReportsController =
+    AutoDisposeAsyncNotifier<List<FinancialReport>>;
+String _$escrowTrackingControllerHash() =>
+    r'9762185bc00ce67d66aaf162439ac4b065595878';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$EscrowTrackingController
+    extends BuildlessAutoDisposeAsyncNotifier<EscrowTracking> {
+  late final String orderNumber;
+
+  FutureOr<EscrowTracking> build(String orderNumber);
+}
+
+/// See also [EscrowTrackingController].
+@ProviderFor(EscrowTrackingController)
+const escrowTrackingControllerProvider = EscrowTrackingControllerFamily();
+
+/// See also [EscrowTrackingController].
+class EscrowTrackingControllerFamily
+    extends Family<AsyncValue<EscrowTracking>> {
+  /// See also [EscrowTrackingController].
+  const EscrowTrackingControllerFamily();
+
+  /// See also [EscrowTrackingController].
+  EscrowTrackingControllerProvider call(String orderNumber) {
+    return EscrowTrackingControllerProvider(orderNumber);
+  }
+
+  @override
+  EscrowTrackingControllerProvider getProviderOverride(
+    covariant EscrowTrackingControllerProvider provider,
+  ) {
+    return call(provider.orderNumber);
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'escrowTrackingControllerProvider';
+}
+
+/// See also [EscrowTrackingController].
+class EscrowTrackingControllerProvider
+    extends
+        AutoDisposeAsyncNotifierProviderImpl<
+          EscrowTrackingController,
+          EscrowTracking
+        > {
+  /// See also [EscrowTrackingController].
+  EscrowTrackingControllerProvider(String orderNumber)
+    : this._internal(
+        () => EscrowTrackingController()..orderNumber = orderNumber,
+        from: escrowTrackingControllerProvider,
+        name: r'escrowTrackingControllerProvider',
+        debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+            ? null
+            : _$escrowTrackingControllerHash,
+        dependencies: EscrowTrackingControllerFamily._dependencies,
+        allTransitiveDependencies:
+            EscrowTrackingControllerFamily._allTransitiveDependencies,
+        orderNumber: orderNumber,
+      );
+
+  EscrowTrackingControllerProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.orderNumber,
+  }) : super.internal();
+
+  final String orderNumber;
+
+  @override
+  FutureOr<EscrowTracking> runNotifierBuild(
+    covariant EscrowTrackingController notifier,
+  ) {
+    return notifier.build(orderNumber);
+  }
+
+  @override
+  Override overrideWith(EscrowTrackingController Function() create) {
+    return ProviderOverride(
+      origin: this,
+      override: EscrowTrackingControllerProvider._internal(
+        () => create()..orderNumber = orderNumber,
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        orderNumber: orderNumber,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeAsyncNotifierProviderElement<
+    EscrowTrackingController,
+    EscrowTracking
+  >
+  createElement() {
+    return _EscrowTrackingControllerProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is EscrowTrackingControllerProvider &&
+        other.orderNumber == orderNumber;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, orderNumber.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin EscrowTrackingControllerRef
+    on AutoDisposeAsyncNotifierProviderRef<EscrowTracking> {
+  /// The parameter `orderNumber` of this provider.
+  String get orderNumber;
+}
+
+class _EscrowTrackingControllerProviderElement
+    extends
+        AutoDisposeAsyncNotifierProviderElement<
+          EscrowTrackingController,
+          EscrowTracking
+        >
+    with EscrowTrackingControllerRef {
+  _EscrowTrackingControllerProviderElement(super.provider);
+
+  @override
+  String get orderNumber =>
+      (origin as EscrowTrackingControllerProvider).orderNumber;
+}
+
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
