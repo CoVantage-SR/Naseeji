e5f59d4f5204a7d0603bfec684babd47cc9a18c4
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-07-12 23:15:04 +0300

    feat: add reusable AppTextField, AppPasswordField, and SearchField components

---
+++ naseeji_factory/lib/core/widgets/app_text_fields.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+import '../constants/app_radius.dart';
+
+class AppTextField extends StatelessWidget {
+  final String labelText;
+  final String? hintText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final bool readOnly;
+  final VoidCallback? onTap;
+
+  const AppTextField({
+    super.key,
+    required this.labelText,
+    this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.controller,
+    this.validator,
+    this.readOnly = false,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      readOnly: readOnly,
+      onTap: onTap,
+      validator: validator,
+      textAlign: TextAlign.right, // RTL align
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
+
+class AppPasswordField extends StatefulWidget {
+  final String labelText;
+  final String? hintText;
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+
+  const AppPasswordField({
+    super.key,
+    this.labelText = 'كلمة المرور',
+    this.hintText,
+    this.controller,
+    this.validator,
+  });
+
+  @override
+  State<AppPasswordField> createState() => _AppPasswordFieldState();
+}
+
+class _AppPasswordFieldState extends State<AppPasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+      labelText: widget.labelText,
+      hintText: widget.hintText,
+      controller: widget.controller,
+      validator: widget.validator,
+      obscureText: _obscureText,
+      keyboardType: TextInputType.visiblePassword,
+      prefixIcon: const Icon(Icons.lock_outline_rounded),
+      suffixIcon: IconButton(
+        icon: Icon(
+          _obscureText ? Icons.visibility_off_rounded : Icons.visibility_rounded,
+          color: AppColors.textSecondaryLight,
+        ),
+        onPressed: () {
+          setState(() {
+            _obscureText = !_obscureText;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final VoidCallback? onFilterTap;
+
+  const SearchField({
+    super.key,
+    this.hintText = 'بحث...',
+    this.controller,
+    this.onChanged,
+    this.onFilterTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onChanged: onChanged,
+      textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: const Icon(Icons.search_rounded, color: AppColors.primary),
+        suffixIcon: onFilterTap != null
+            ? IconButton(
+                icon: const Icon(Icons.tune_rounded, color: AppColors.primary),
+                onPressed: onFilterTap,
+              )
+            : null,
+        filled: true,
+        fillColor: Theme.of(context).cardColor,
+        border: OutlineInputBorder(
+          borderRadius: AppRadius.rMD,
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: AppRadius.rMD,
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: AppRadius.rMD,
+          borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
+        ),
+      ),
+    );
+  }
+}
