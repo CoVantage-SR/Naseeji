197841337a1caa711cdc1aeb916372d3b20c3ed3
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-07-13 06:27:47 +0300

    feat: migrate Android project configuration to Kotlin DSL with updated build settings

--- naseeji_factory/android/app/build.gradle.kts
+++ naseeji_factory/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.naseeji_factory"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- naseeji_factory/android/build.gradle.kts
+++ naseeji_factory/android/build.gradle.kts
@@ -19,11 +19,42 @@ subprojects {
         val android = project.extensions.findByName("android")
         if (android != null) {
             try {
+                val setCompileSdk = android.javaClass.getMethod("setCompileSdk", java.lang.Integer::class.java)
+                setCompileSdk.invoke(android, 36)
+            } catch (e: Exception) {
+                try {
+                    val setCompileSdk = android.javaClass.getMethod("setCompileSdk", Int::class.java)
+                    setCompileSdk.invoke(android, 36)
+                } catch (e2: Exception) {
+                    try {
+                        val compileSdkVersion = android.javaClass.getMethod("compileSdkVersion", Int::class.java)
+                        compileSdkVersion.invoke(android, 36)
+                    } catch (e3: Exception) {
+                        // Ignore
+                    }
+                }
+            }
+
+            try {
                 val getNamespace = android.javaClass.getMethod("getNamespace")
                 val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
                 if (getNamespace.invoke(android) == null) {
                     val packageName = "dev.isar.${project.name.replace("_", "").replace("-", "")}"
                     setNamespace.invoke(android, packageName)
+                }
+            } catch (e: Exception) {
+                // Ignore
+            }
+
+            try {
+                val manifestFile = project.file("src/main/AndroidManifest.xml")
+                if (manifestFile.exists()) {
+                    val content = manifestFile.readText()
+                    if (content.contains("package=")) {
+                        manifestFile.setWritable(true)
+                        val cleanedContent = content.replace(Regex("""package="[^"]*""""), "")
+                        manifestFile.writeText(cleanedContent)
+                    }
                 }
             } catch (e: Exception) {
                 // Ignore
