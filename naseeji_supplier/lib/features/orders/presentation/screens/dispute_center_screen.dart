2d1f6080619bf2d8d9dcc8caa61e47c60e10ed74
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-07-22 14:04:24 +0300

    feat: add shipment issue reporting, dispute center, and shipping details views

--- naseeji_supplier/lib/features/orders/presentation/screens/dispute_center_screen.dart
+++ naseeji_supplier/lib/features/orders/presentation/screens/dispute_center_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:naseeji_supplier/core/theme/app_colors.dart';
