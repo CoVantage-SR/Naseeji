49475facd1df35778d98697105ccfed376fef434
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-08-01 00:32:35 +0300

    feat: implement security settings provider and export moderation service

--- naseeji_factory_supplier/lib/supplier/core/services/moderation/moderation.dart
+++ naseeji_factory_supplier/lib/supplier/core/services/moderation/moderation.dart
@@ -1,11 +1,1 @@
-export 'domain/entities/moderation_enums.dart';
-export 'domain/entities/moderation_result.dart';
-export 'domain/entities/violation_log.dart';
-export 'domain/analyzers/content_analyzer_interface.dart';
-export 'content_moderation_service.dart';
-export 'moderation_provider.dart';
-export 'presentation/content_moderation_dialog.dart';
-
-
-
-
+export 'package:naseeji_factory/core/services/moderation/moderation.dart';
